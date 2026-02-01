6da13d2ae442052583f5a3ef1be89eefd21b1d0c
Computhink-CS145 <cs145@computhink.com.sg>
2026-02-01 04:35:12 +0000

    end of lesson four

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,11 @@
 print("Hello from lesson 3")
+
+seafood = 100
+sidedish = 20
+bowlofrice = 2.50
+
+total = (seafood * 5) + (sidedish * 8) + (bowlofrice * 20)
+total1 = total + (total * (10/100))
+total2 = total1 + (total1 * (9/100))
+
+print(round(total2,1))
---
+++ CT06_04/sunday.py
@@ -0,0 +1,18 @@
+# name = (input("What is your name? "))
+# print(name)
+# colour = (input("What is your favourite colour? "))
+# print(colour)
+
+# name = "CaseOh"
+# message = "I am yery hungry"
+# print(f"{name} says '{message}'")
+
+# hobby = input("What is your hobby? ")
+# print(f"I enjoy {hobby}.")
+
+# qty = 3
+# amt = 17.95
+# print("plz pay me $" , round(qty * amt))
+var = int(input("how old are you? ") )
+
+print("Next year, you will be " + str( var+ 1) )
